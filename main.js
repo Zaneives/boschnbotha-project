bbda3650705e1a4e55c4f1791a96678f07ccd8af
Zane Ives <deni23n@gmail.com>
2022-10-13 22:23:34 +0200

    picture carousel and kinda fixed icon

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ navToggle.addEventListener("click", () => {
     iconHamburger.toggleAttribute("data-visible");
     iconClose.toggleAttribute("data-visible");
 })
+
+
+// commment this out if you don't want picture carousel
+let counter = 1
+const tester = document.querySelector(".main-content")
+function looper() {
+    tester.style.backgroundImage = `url("./images/image${counter}.jpg")`
+    counter = (counter)%5 + 1
+}
+looper()
+setInterval(function(){looper()},20000)
