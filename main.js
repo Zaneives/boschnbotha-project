15a97d6d1c99ce8fca4ff5c1de6b4363589d1724
Zane Ives <deni23n@gmail.com>
2022-10-13 13:09:24 +0200

    mobile nav mostly done.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-nav");
-// const primaryNav = document.querySelector(".primary-nav");
-// const primaryNav = document.querySelector(".primary-nav");
+const iconHamburger = document.querySelector(".icon-hamburger");
+const iconClose = document.querySelector(".icon-close");
 navToggle.addEventListener("click", () => {
     primaryNav.toggleAttribute("data-visible");
+    iconHamburger.toggleAttribute("data-visible");
+    iconClose.toggleAttribute("data-visible");
 })
