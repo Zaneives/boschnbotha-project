f9cfe4276014554e5c787952a4b773cd6074df68
Zane Ives <deni23n@gmail.com>
2022-10-27 17:12:11 +0200

    added activities page and carousel v2

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ navToggle.addEventListener("click", () => {
     primaryNav.toggleAttribute("data-visible");
     iconHamburger.toggleAttribute("data-visible");
     iconClose.toggleAttribute("data-visible");    
+// *these are the tweaks to get the transitions
     if (primaryNav.hasAttribute("data-visible")) {
         setTimeout(function(){
             openNav.style.top = "0px";
@@ -16,6 +17,25 @@ navToggle.addEventListener("click", () => {
     }
 })
 
+
+// *alternate menu button. The original one seems to work just fine with the new tweaks.
+// console.log(primaryNav.hasAttribute("data-visible"))
+// function toggle() {
+    //     if (openNav.style.top == "-20rem") {
+        //         openNav.style.display = "block"    
+//         setTimeout(function(){
+//             openNav.style.top = "0px";    
+//         }, 50);
+
+//     } else {
+    //         openNav.style.top = "-20rem";    
+    //         setTimeout(function(){
+        //             openNav.style.display = "none"    
+        //         }, 300);
+        
+        //     }
+        // }
+        
 // *commment this out if you don't want picture carousel
 // let counter = 1
 // const tester = document.querySelector(".main-content")
@@ -26,20 +46,12 @@ navToggle.addEventListener("click", () => {
 // looper()
 // setInterval(function(){looper()},20000)
 
-// *alternate menu button. The original one seems to work just fine with the new tweaks.
-// console.log(primaryNav.hasAttribute("data-visible"))
-// function toggle() {
-//     if (openNav.style.top == "-20rem") {
-//         openNav.style.display = "block"    
-//         setTimeout(function(){
-//             openNav.style.top = "0px";    
-//         }, 50);
-        
-//     } else {
-//         openNav.style.top = "-20rem";    
-//         setTimeout(function(){
-//             openNav.style.display = "none"    
-//         }, 300);
-        
-//     }
-// }
+// *carousel v2
+let counter = 1
+const tester = document.querySelector(".main-image img")
+function looper() {
+    tester.src = `./images/image${counter}.webp`
+    counter = (counter)%5 + 1
+}
+looper()
+setInterval(function(){looper()},20000)
