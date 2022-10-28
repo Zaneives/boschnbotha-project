4e84d643e05a60653c283c2bc702fd818c07a995
Zane Ives <deni23n@gmail.com>
2022-10-28 21:41:20 +0200

    carousel v3, with a transition

--- main.js
+++ main.js
@@ -17,24 +17,21 @@ navToggle.addEventListener("click", () => {
     }
 })
 
-
 // *alternate menu button. The original one seems to work just fine with the new tweaks.
 // console.log(primaryNav.hasAttribute("data-visible"))
 // function toggle() {
-    //     if (openNav.style.top == "-20rem") {
-        //         openNav.style.display = "block"    
+//     if (openNav.style.top == "-20rem") {
+//             openNav.style.display = "block"    
 //         setTimeout(function(){
 //             openNav.style.top = "0px";    
 //         }, 50);
-
 //     } else {
-    //         openNav.style.top = "-20rem";    
-    //         setTimeout(function(){
-        //             openNav.style.display = "none"    
-        //         }, 300);
-        
-        //     }
-        // }
+//         openNav.style.top = "-20rem";    
+//         setTimeout(function(){
+//                 openNav.style.display = "none"    
+//             }, 300);    
+//     }
+// }
         
 // *commment this out if you don't want picture carousel
 // let counter = 1
@@ -47,11 +44,41 @@ navToggle.addEventListener("click", () => {
 // setInterval(function(){looper()},20000)
 
 // *carousel v2
-let counter = 1
-const tester = document.querySelector(".main-image img")
-function looper() {
-    tester.src = `./images/image${counter}.webp`
-    counter = (counter)%5 + 1
+// let counter = 1
+// const tester = document.querySelector(".main-images img")
+// function looper() {
+//     tester.src = `./images/image${counter}.webp`
+//     counter = (counter)%5 + 1
+// }
+// looper()
+// setInterval(function(){looper()},20000)
+    
+// *carousel v3
+const carouselImages = document.querySelectorAll(".main-images img")
+let counter = 0;
+let numberOfImages = 5;     // set number of images here
+carouselImages[0].style.display="block"
+carouselImages[0].style.filter="opacity(0.4)"
+
+function carouselLoop() {
+    carouselImages[counter].style.filter="opacity(0.1)"
+    carouselImages[counter].style.display="none"
+    counter = (counter + 1)%numberOfImages
+    carouselImages[counter].style.display="block"
+    setTimeout(() => {
+        carouselImages[counter].style.filter="opacity(0.4)"    
+    }, 50);
 }
-looper()
-setInterval(function(){looper()},20000)
+setInterval(() => carouselLoop(), 10000)
+
+
+// setInterval(() => {
+//     carouselImages[0].style.display="none"
+//     carouselImages[1].style.filter="opacity(0.1)"    
+//     carouselImages[1].style.display="block"
+//     setTimeout(() => {
+//         carouselImages[1].style.filter="opacity(0.4)"    
+//     }, 50);
+// }, 5000)
+
+// carouselImages[4].style.display="block"
